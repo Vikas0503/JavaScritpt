ec47f21c91834398f087ceaad1e8ba97275d2abc
vikas0503 <bhanwarvikaskumar@gmail.com>
2026-05-05 20:52:36 +0530

    added day2

---
+++ js/Tasks/day2tasks.js
@@ -0,0 +1,24 @@
+const firstName = "john wick";
+const age =24;
+const isApproved = true;
+const number = null;
+let city;
+
+console.log(firstName);
+console.log(typeof firstName);
+
+console.log(age);
+console.log(typeof age);
+
+console.log(isApproved);
+console.log(typeof isApproved);
+
+console.log(number);
+console.log(typeof number);
+
+console.log(city);
+console.log(typeof city);
+
+const myName = "    john wick   ";
+console.log(myName.trim().toUpperCase().length);
+console.log(myName.includes("gouri"));
---
+++ js/classroom/day2.js
@@ -0,0 +1,18 @@
+console.log("Hello JavaScript");
+console.log(42);
+console.log(3.14);
+console.log(true);
+console.log("Name:Rahul","Age:25");
+
+const name = "john";
+console.log(name);
+
+let score=0;
+score=10;
+score=score+5;
+console.log(score);
+
+let city;
+console.log(city);
+city="kochi";
+console.log(city);
